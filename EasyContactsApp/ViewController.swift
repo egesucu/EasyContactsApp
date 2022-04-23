75c6284a255156910ac98669026720ebf8f257d7
Ege Sucu <egesucu@icloud.com>
2022-04-23 15:36:18 +0300

    Initial Commit

---
+++ EasyContactsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EasyContactsApp
+//
+//  Created by Ege Sucu on 23.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
